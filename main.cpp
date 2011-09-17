76a23854ac7274b561f401c440484780ca656972
Takashi Imagire <imagire@gmail.com>
2011-09-17 22:29:04 +0900

    first commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+
+int add(int x, int y)
+{
+    return x + y;
+}
+
+TEST(AddTest, Test1)
+{
+    EXPECT_EQ(2, add(1, 1));
+}
+
+TEST(AddTest, Test2)
+{
+    EXPECT_EQ(3, add(1, 2));
+}
+
+int main(int argc, char* argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+
+	int ret = RUN_ALL_TESTS();
+
+	return ret;
+}
