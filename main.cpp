e154302b12e977f926d6e84e11f8351e54be06d4
Takashi Imagire <imagire@nifty.com>
2011-10-08 05:12:35 +0900

    Study 1

--- main.cpp
+++ main.cpp
@@ -1,18 +1,51 @@
 #include <gtest/gtest.h>
 
-int add(int x, int y)
-{
-    return x + y;
-}
+class Norikae{
+public:
+	enum Station{
+		YOKOHAMA,
+		OOMIYA,
+		OHSHIMA,
 
-TEST(AddTest, Test1)
-{
-    EXPECT_EQ(2, add(1, 1));
-}
+		END
+	};
 
-TEST(AddTest, Test2)
+private:
+	struct Edge{
+		Station st1;
+		Station st2;
+	};
+
+	static const Edge aEdge[];
+
+public:
+	Norikae(){}
+	~Norikae(){}
+
+	static bool IsConnected(Station from, Station to)
+	{
+		const Edge *p = aEdge;
+
+		while(p->st1 != END){
+			if(p->st1 == from && p->st2 == to) return true;
+			if(p->st2 == from && p->st1 == to) return true;
+			p++;
+		}
+
+		return false;
+	}
+};
+
+const Norikae::Edge Norikae::aEdge[] = {
+	{Norikae::OOMIYA, Norikae::YOKOHAMA},
+	{Norikae::END, Norikae::END},
+	};
+
+TEST(NorikaeTest1, IsConnected)
 {
-    EXPECT_EQ(3, add(1, 2));
+	EXPECT_EQ(true, Norikae::IsConnected(Norikae::OOMIYA, Norikae::YOKOHAMA));
+	EXPECT_EQ(false, Norikae::IsConnected(Norikae::OOMIYA, Norikae::OHSHIMA));
+	EXPECT_EQ(false, Norikae::IsConnected(Norikae::YOKOHAMA, Norikae::OHSHIMA));
 }
 
 int main(int argc, char* argv[])
