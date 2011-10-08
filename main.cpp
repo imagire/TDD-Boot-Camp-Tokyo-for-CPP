6769c109f59a8ea0e80cdcad5dc7e6d3dd25fe9c
hazukit <hazukit0@gmail.com>
2011-10-08 11:17:19 +0900

    add checkTrainRoute method

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <gtest/gtest.h>
 
+typedef enum {
+	OMIYA,
+	YOKOHAMA,
+	OSHIMA
+} STATION;
+
+extern bool checkTrainRoute(STATION station1, STATION station2);
+
 int add(int x, int y)
 {
     return x + y;
@@ -15,6 +23,21 @@ TEST(AddTest, Test2)
     EXPECT_EQ(3, add(1, 2));
 }
 
+TEST(checkTrainRouteTest, Test1)
+{
+    EXPECT_TRUE(checkTrainRoute(OMIYA,YOKOHAMA));
+    EXPECT_TRUE(checkTrainRoute(YOKOHAMA,OMIYA));
+}
+
+TEST(checkTrainRouteTest, Test2)
+{
+	EXPECT_FALSE(checkTrainRoute(OSHIMA,YOKOHAMA));
+	EXPECT_FALSE(checkTrainRoute(YOKOHAMA,OSHIMA));
+    EXPECT_FALSE(checkTrainRoute(OMIYA,OMIYA));
+    EXPECT_FALSE(checkTrainRoute(YOKOHAMA,YOKOHAMA));
+    EXPECT_FALSE(checkTrainRoute(OSHIMA,OSHIMA));
+}
+
 int main(int argc, char* argv[])
 {
     ::testing::InitGoogleTest(&argc, argv);
@@ -22,4 +45,23 @@ int main(int argc, char* argv[])
 	int ret = RUN_ALL_TESTS();
 
 	return ret;
+}
+
+bool checkTrainRoute(STATION station1, STATION station2)
+{
+	if (station1 == OMIYA)
+	{
+		if (station2 == YOKOHAMA)
+		{
+			return true;
+		}
+	}
+	else if (station2 == OMIYA)
+	{
+		if (station1 == YOKOHAMA)
+		{
+			return true;
+		}
+	}
+	return false;
 }
