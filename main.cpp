8224eb63d48fc470162d82f14f0af2a0fcd8aa25
daidai <sacred.pray@gmail.com>
2011-10-08 11:08:08 +0900

    課題1完了

--- main.cpp
+++ main.cpp
@@ -14,6 +14,32 @@ TEST(AddTest, Test2)
 {
     EXPECT_EQ(3, add(1, 2));
 }
+enum BASYO {
+	OOMIYA = 0,
+	YOKOHAMA,
+	OOSIMA
+};
+
+bool IsToutyaku(enum BASYO start, enum BASYO end)
+{
+	bool ret=false;
+	if (start == OOMIYA && end == YOKOHAMA) {
+		ret = true;
+	}
+	else if (start == OOMIYA && end == YOKOHAMA) {
+		ret = false;
+	}
+	else {
+		ret = false;
+	}
+	return ret;
+}
+
+TEST(Work1, Test1)
+{
+	EXPECT_EQ(true, IsToutyaku(OOMIYA, YOKOHAMA));
+	EXPECT_EQ(false, IsToutyaku(OOSIMA, YOKOHAMA));
+}
 
 int main(int argc, char* argv[])
 {
