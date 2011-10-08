80f01bc5f27e57874c75925e6949b9719b8beb22
Haruto Watanabe <dailioh@gmail.com>
2011-10-08 14:01:20 +0900

    課題4終了

--- main.cpp
+++ main.cpp
@@ -1,29 +1,63 @@
 #include <gtest/gtest.h>
 
-enum Station
+namespace st
 {
-	Station_Yokohama,
-	Station_Ohshima,
-	Station_Ohmiya,
-	Station_Tokyo
-};
+	enum Station
+	{
+		Yokohama,
+		Kawasaki,
+		MusashiKosugi,
+		Tokyo,
+		Shibuya,
+		NishiKokubunji,
+		Shinjuku,
+		Ochanomizu,
+		Akihabara,
+		Tabata,
+		Ikebukuro,
+		Akabane,
+		MinamiUrawa,
+		Ohshima,
+		Ohmiya,
+	};
+}
 
 struct Path
 {
-	Station st1;
-	Station st2;
+	st::Station st1;
+	st::Station st2;
 }const good_paths[]=
 {
-	{Station_Tokyo,Station_Yokohama},
-	{Station_Tokyo,Station_Ohmiya},
+	{st::Yokohama,st::Kawasaki},
+	{st::Yokohama,st::MusashiKosugi},
+	{st::Kawasaki,st::MusashiKosugi},
+	{st::Kawasaki,st::Tokyo},
+	{st::MusashiKosugi,st::Shibuya},
+	{st::MusashiKosugi,st::NishiKokubunji},
+	{st::Tokyo,st::Shibuya},
+	{st::Tokyo,st::Akihabara},
+	{st::Tokyo,st::Ochanomizu},
+	{st::Shibuya,st::Shinjuku},
+	{st::Akihabara,st::Ochanomizu},
+	{st::Akihabara,st::Tabata},
+	{st::Ochanomizu,st::Shinjuku},
+	{st::Shinjuku,st::NishiKokubunji},
+	{st::Shinjuku,st::Ikebukuro},
+	{st::Tabata,st::Ikebukuro},
+	{st::Tabata,st::Akabane},
+	{st::Ikebukuro,st::Akabane},
+	{st::NishiKokubunji,st::MinamiUrawa},
+	{st::Akabane,st::MinamiUrawa},
+	{st::MinamiUrawa,st::Ohmiya},
 };
 
 #define Elements(a) (sizeof(a)/sizeof(a[0]))
 
 int path_stack[100];
 
-bool is_reach(Station station1, Station station2 , int stack_position=0)
+bool is_reach(st::Station station1, st::Station station2 , int stack_position=0)
 {
+
 	for(int n=0;n<Elements(good_paths);n++)
 	{
 		if(good_paths[n].st1 == station1 && good_paths[n].st2 == station2)
@@ -51,36 +85,59 @@ bool is_reach(Station station1, Station station2 , int stack_position=0)
 		if(good_paths[n].st1 == station1 )
 		{
 			path_stack[stack_position] = n;
-			return is_reach(good_paths[n].st2 , station2,stack_position + 1);
+			if(is_reach(good_paths[n].st2 , station2,stack_position + 1))
+			{
+				return true;
+			}
+			else
+			{
+				continue;
+			}
 		}
 		if(good_paths[n].st2 == station1 )
 		{
 			path_stack[stack_position] = n;
-			return is_reach(good_paths[n].st1 , station2,stack_position + 1);
+			if(is_reach(good_paths[n].st1 , station2,stack_position + 1))
+			{
+				return true;
+			}
+			else
+			{
+				continue;
+			}
 		}
-
 	}
 	return false;
 }
 
 TEST(AddTest, ForDebug)
 {
-	EXPECT_TRUE(is_reach(Station_Yokohama,Station_Ohmiya));
+	EXPECT_TRUE(is_reach(st::Yokohama,st::Ohmiya));
 }
 
 TEST(AddTest, Exchange)
 {
-	EXPECT_FALSE(is_reach(Station_Yokohama,Station_Ohshima));
-	EXPECT_FALSE(is_reach(Station_Ohshima,Station_Yokohama));
+	EXPECT_FALSE(is_reach(st::Yokohama,st::Ohshima));
+	EXPECT_FALSE(is_reach(st::Ohshima,st::Yokohama));
 
-	EXPECT_TRUE(is_reach(Station_Yokohama,Station_Ohmiya));
-	EXPECT_TRUE(is_reach(Station_Ohmiya,Station_Yokohama));
+	EXPECT_TRUE(is_reach(st::Yokohama,st::Ohmiya));
+	EXPECT_TRUE(is_reach(st::Ohmiya,st::Yokohama));
 
-	EXPECT_TRUE(is_reach(Station_Yokohama,Station_Tokyo));
-	EXPECT_TRUE(is_reach(Station_Tokyo,Station_Yokohama));
+	EXPECT_TRUE(is_reach(st::Yokohama,st::Tokyo));
+	EXPECT_TRUE(is_reach(st::Tokyo,st::Yokohama));
 
-	EXPECT_TRUE(is_reach(Station_Tokyo,Station_Ohmiya));
-	EXPECT_TRUE(is_reach(Station_Ohmiya,Station_Tokyo));
+	EXPECT_TRUE(is_reach(st::Tokyo,st::Ohmiya));
+	EXPECT_TRUE(is_reach(st::Ohmiya,st::Tokyo));
+}
+
+TEST(AddTest, Route)
+{
+	EXPECT_TRUE(is_reach(st::Yokohama,st::Ikebukuro));
+	EXPECT_TRUE(is_reach(st::Yokohama,st::Shibuya));
+	EXPECT_TRUE(is_reach(st::MusashiKosugi,st::Akihabara));
+	EXPECT_TRUE(is_reach(st::Shinjuku,st::Ohmiya));
+	EXPECT_TRUE(is_reach(st::Tokyo,st::Ikebukuro));
+
 }
 
 
