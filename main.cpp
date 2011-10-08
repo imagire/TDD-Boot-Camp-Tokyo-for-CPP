a6ef84c6463b09dc08b86270a7c1245e3f4d82ed
Koichi Tanaka <kou11031@gmail>
2011-10-08 14:07:49 +0900

    finished #2

--- main.cpp
+++ main.cpp
@@ -1,18 +1,101 @@
 #include <gtest/gtest.h>
+#include <string>
 
-int add(int x, int y)
+enum Station
 {
-    return x + y;
+  yokohama,
+  oomiya,
+  ooshima,
+  tokyo
+};
+
+struct RootSet
+{
+  Station tar;
+  Station dist;
+  RootSet(Station _tar, Station _dist): tar(_tar), dist(_dist) {};
+};
+
+bool canGo(RootSet set)
+{
+  bool ret = false;
+
+  if(yokohama == set.tar)
+  {
+    if(oomiya == set.dist
+      || tokyo == set.dist)
+      ret = true;
+  }
+  else if(tokyo == set.tar)
+  {
+    if(yokohama == set.dist || oomiya == set.dist)
+    {
+      return true;
+    }
+  }
+  else if(oomiya == set.tar)
+  {
+    if(tokyo == set.dist)
+    {
+      return true;
+    }
+  }
+
+  return ret;
 }
 
-TEST(AddTest, Test1)
+/*
+ * ‰¡•l->‘å‹{‚ÍŒq‚ª‚Á‚Ä‚¢‚é
+ */
+TEST(TrainTest, YokohamaToOomiya)
 {
-    EXPECT_EQ(2, add(1, 1));
+  RootSet rootSet(yokohama, oomiya);
+  EXPECT_TRUE(canGo(rootSet));
 }
 
-TEST(AddTest, Test2)
+/*
+ * ‰¡•l->‘å“‡‚Í‚Â‚È‚ª‚Á‚Ä‚¢‚È‚¢
+ */
+TEST(TrainTest, YokohamaNotToOoshima)
 {
-    EXPECT_EQ(3, add(1, 2));
+  RootSet rootSet(yokohama, ooshima);
+  EXPECT_FALSE(canGo(rootSet));
+}
+
+/*
+ * ‰¡•l->“Œ‹ž‚ÍŒq‚ª‚Á‚Ä‚¢‚é
+ */
+TEST(TrainTest, YokohamaToTokyo)
+{
+  RootSet rootSet(yokohama, tokyo);
+  EXPECT_TRUE(canGo(rootSet));
+}
+
+/*
+ * “Œ‹ž ->‰¡•l‚ÍŒq‚ª‚Á‚Ä‚¢‚é
+ */
+TEST(TrainTest, TokyoToYokohama)
+{
+  RootSet rootSet(tokyo, yokohama);
+  EXPECT_TRUE(canGo(rootSet));
+}
+
+/*
+ * “Œ‹ž->‘å‹{‚ÍŒq‚ª‚Á‚Ä‚¢‚é
+ */
+TEST(TrainTest, TokyoToOomiya)
+{
+  RootSet rootSet(tokyo, oomiya);
+  EXPECT_TRUE(canGo(rootSet));
+}
+
+/*
+ * ‘å‹{->“Œ‹ž‚ÍŒq‚ª‚Á‚Ä‚¢‚é
+ */
+TEST(TrainTest, OomiyaToTokyo)
+{
+  RootSet rootSet(oomiya, tokyo);
+  EXPECT_TRUE(canGo(rootSet));
 }
 
 int main(int argc, char* argv[])
