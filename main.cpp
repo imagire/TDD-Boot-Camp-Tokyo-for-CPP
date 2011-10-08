59f9a779a3f5f3ddd5ad2390d6d4171599db4a18
Yohei Onishi <vivre214@gmail.com>
2011-10-08 14:21:23 +0900

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,72 +1,13 @@
 #include <gtest/gtest.h>
+#include "Train.h"
 
-int add(int x, int y)
+TEST(Train, Problem1)
 {
-    return x + y;
-}
-
-TEST(AddTest, Test1)
-{
-    EXPECT_EQ(2, add(1, 1));
-}
-
-TEST(AddTest, Test2)
-{
-    EXPECT_EQ(3, add(1, 2));
-}
-
-int main(int argc, char* argv[])
-{
-    ::testing::InitGoogleTest(&argc, argv);
-
-	int ret = RUN_ALL_TESTS();
-
-	return ret;
-}
-
-enum TRAIN_STATION
-{
-	OMIYA = 0,
-	YOKOHAMA,
-	TOKYO,
-	OSHIMA
-};
-
-bool cango(TRAIN_STATION start, TRAIN_STATION end)
-{
-
-	if(start == OMIYA)
-	{
-		if((end == YOKOHAMA) || (end == TOKYO))
-		{
-			return true;
-		}
-	}
-
-	if(start == TOKYO)
-	{
-		if((end == OMIYA)||(end == YOKOHAMA))
-		{
-			return true;
-		}
-	}
-
-
-	if(start == YOKOHAMA)
-	{
-		if((end == OMIYA)||(end == TOKYO))
-		{
-			return true;
-		}
-	}
-
-	return false;
-}
-
-TEST(Train, Move)
-{
-	EXPECT_TRUE(cango(OMIYA,YOKOHAMA));
-	EXPECT_FALSE(cango(OSHIMA,YOKOHAMA));
+	EXPECT_TRUE (cango(YOKOHAMA,   OMIYA));
+	EXPECT_TRUE (cango(OMIYA,      YOKOHAMA));
+	EXPECT_FALSE(cango(YOKOHAMA,   OSHIMA));
+	EXPECT_FALSE(cango(OSHIMA,     YOKOHAMA));
+	EXPECT_FALSE(cango(OSHIMA,     OMIYA));
 }
 
 TEST(Train, Problem2)
@@ -74,7 +15,7 @@ TEST(Train, Problem2)
 	EXPECT_TRUE(cango(YOKOHAMA,TOKYO));
 	EXPECT_TRUE(cango(TOKYO,YOKOHAMA));
 	EXPECT_TRUE(cango(TOKYO,OMIYA));
-	EXPECT_TRUE(cango(OMIYA,TOKYO));	
+	EXPECT_TRUE(cango(OMIYA,TOKYO));
 }
 
 TEST(Train, Problem3)
