7991592c5e6a5bf812af8df6e3f6ce580ffb2784
daidai <sacred.pray@gmail.com>
2011-10-08 11:32:12 +0900

    課題2完了。

--- main.cpp
+++ main.cpp
@@ -17,20 +17,26 @@ TEST(AddTest, Test2)
 enum BASYO {
 	OOMIYA = 0,
 	YOKOHAMA,
-	OOSIMA
+	OOSIMA,
+	TOKYO
+};
+BASYO ikeru[][2] = {
+	{OOMIYA,YOKOHAMA},
+	{YOKOHAMA, OOMIYA},
+	{TOKYO, YOKOHAMA},
+	{YOKOHAMA, TOKYO},
+	{TOKYO, OOMIYA},
+	{OOMIYA, TOKYO}
 };
 
 bool IsToutyaku(enum BASYO start, enum BASYO end)
 {
 	bool ret=false;
-	if (start == OOMIYA && end == YOKOHAMA) {
-		ret = true;
-	}
-	else if (start == OOMIYA && end == YOKOHAMA) {
-		ret = false;
-	}
-	else {
-		ret = false;
+	for(int i = 0; i < sizeof(ikeru)/sizeof(ikeru[0]) ; ++i){
+		if (start == ikeru[i][0] && end == ikeru[i][1]) {
+			ret = true;
+			break;
+		}
 	}
 	return ret;
 }
@@ -38,7 +44,17 @@ bool IsToutyaku(enum BASYO start, enum BASYO end)
 TEST(Work1, Test1)
 {
 	EXPECT_EQ(true, IsToutyaku(OOMIYA, YOKOHAMA));
+	EXPECT_EQ(true, IsToutyaku(YOKOHAMA, OOMIYA));
 	EXPECT_EQ(false, IsToutyaku(OOSIMA, YOKOHAMA));
+	EXPECT_EQ(false, IsToutyaku(YOKOHAMA, OOSIMA));
+}
+
+TEST(Work2, Test1)
+{
+	EXPECT_EQ(true, IsToutyaku(YOKOHAMA, TOKYO));
+	EXPECT_EQ(true, IsToutyaku(TOKYO, YOKOHAMA));
+	EXPECT_EQ(true, IsToutyaku(OOMIYA, TOKYO));
+	EXPECT_EQ(true, IsToutyaku(TOKYO, OOMIYA));
 }
 
 int main(int argc, char* argv[])
