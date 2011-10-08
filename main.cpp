587a3c5dae8bf075aabd947050d631c966664fd5
sho <sho@example.com>
2011-10-08 16:12:39 +0900

    kadai4 green

--- main.cpp
+++ main.cpp
@@ -20,38 +20,82 @@ enum {
 };
 
 int nextStation[ENUM_STATION_SIZE][ENUM_STATION_SIZE] = {0};
+bool ashiato[ENUM_STATION_SIZE] = {false};
 
-bool ashiato[ENUM_STATION_SIZE][ENUM_STATION_SIZE] = {false};
+void initAshiato(void){
+	for(int i = 0; i < ENUM_STATION_SIZE; i++) ashiato[i] = false;
+}
+void setUp(void){
+		initAshiato();
+
+		nextStation[OOMIYA][YOKOHAMA] = 1;
+		nextStation[YOKOHAMA][OOMIYA] = 1;
+		nextStation[OOMIYA][TOKYO] = 1;
+		nextStation[TOKYO][OOMIYA] = 1;
+		nextStation[OOMIYA][MINAMIURAWA] = 1;
+		nextStation[MINAMIURAWA][OOMIYA] = 1;
+		nextStation[MINAMIURAWA][AKABANE] = 1;
+		nextStation[MINAMIURAWA][NISHIKOKUBUNJI] = 1;
+		nextStation[AKABANE][MINAMIURAWA] = 1;
+		nextStation[AKABANE][IKEBUKURO] = 1;
+		nextStation[AKABANE][TABATA] = 1;
+		nextStation[NISHIKOKUBUNJI][MINAMIURAWA] = 1;
+		nextStation[NISHIKOKUBUNJI][SHINJUKU] = 1;
+		nextStation[NISHIKOKUBUNJI][MUSASHIKOSUGI] = 1;
+		nextStation[IKEBUKURO][AKABANE] = 1;
+		nextStation[IKEBUKURO][SHINJUKU] = 1;
+		nextStation[IKEBUKURO][TABATA] = 1;
+		nextStation[TABATA][AKABANE] = 1;
+		nextStation[TABATA][IKEBUKURO] = 1;
+		nextStation[TABATA][AKIHABARA] = 1;
+		nextStation[SHINJUKU][IKEBUKURO] = 1;
+		nextStation[SHINJUKU][NISHIKOKUBUNJI] = 1;
+		nextStation[SHINJUKU][OCHANOMIZU] = 1;
+		nextStation[SHINJUKU][SHIBUYA] = 1;
+		nextStation[OCHANOMIZU][SHINJUKU] = 1;
+		nextStation[OCHANOMIZU][AKIHABARA] = 1;
+		nextStation[OCHANOMIZU][TOKYO] = 1;
+		nextStation[AKIHABARA][TOKYO] = 1;
+		nextStation[AKIHABARA][OCHANOMIZU] = 1;
+		nextStation[AKIHABARA][TABATA] = 1;
+		nextStation[SHIBUYA][SHINJUKU] = 1;
+		nextStation[SHIBUYA][TOKYO] = 1;
+		nextStation[SHIBUYA][MUSASHIKOSUGI] = 1;
+		nextStation[TOKYO][KAWASAKI] = 1;
+		nextStation[TOKYO][SHIBUYA] = 1;
+		nextStation[TOKYO][AKIHABARA] = 1;
+		nextStation[TOKYO][OCHANOMIZU] = 1;
+		nextStation[MUSASHIKOSUGI][SHIBUYA] = 1;
+		nextStation[MUSASHIKOSUGI][KAWASAKI] = 1;
+		nextStation[MUSASHIKOSUGI][NISHIKOKUBUNJI] = 1;
+		nextStation[MUSASHIKOSUGI][YOKOHAMA] = 1;
+		nextStation[KAWASAKI][MUSASHIKOSUGI] = 1;
+		nextStation[KAWASAKI][TOKYO] = 1;
+		nextStation[KAWASAKI][YOKOHAMA] = 1;
+		nextStation[YOKOHAMA][KAWASAKI] = 1;
+		nextStation[YOKOHAMA][MUSASHIKOSUGI] = 1;		
+}
 
 class Train {
 public:
-	Train () {};
+	Train () {setUp();};
 	bool able(int from, int to) {
 		bool result = false;
-		nextStation[OOMIYA][YOKOHAMA] = 1;
-		nextStation[YOKOHAMA][OOMIYA] = 1;
-		nextStation[YOKOHAMA][OOSHIMA] = 0;
-		nextStation[YOKOHAMA][TOKYO] = 1;
-		nextStation[TOKYO][OOMIYA] = 1;
-		nextStation[TOKYO][YOKOHAMA] = 1;
-		nextStation[OOMIYA][TOKYO] = 1;
-		nextStation[AKIHABARA][KAWASAKI] = 1;
-		nextStation[KAWASAKI][TOKYO] = 1;
-		nextStation[KAWASAKI][AKIHABARA] = 1;
-		nextStation[TOKYO][KAWASAKI] = 1;
-		nextStation[AKIHABARA][KAWASAKI] = 1;
-
+		ashiato[from] = true;
 		for(int i= 0; i < ENUM_STATION_SIZE; i++){
-			ashiato[from][i] = true;
 			if(nextStation[from][i] == 1){
 				if(i == to) {
 					result = true;
 					break;
 				}
 				else{
-					if(ashiato[from][i] == false)
+					if(ashiato[i] == false)
 					{
-					result = able(i, to);
+						result = able(i, to);
+						if (result == true)
+						{
+							break;
+						}
 					}
 				}
 			}
@@ -67,7 +111,8 @@ TEST(TrainTest, Test1)
 {
 	Train train;
 
-//	EXPECT_TRUE(train.able(OOMIYA, YOKOHAMA));
+	EXPECT_TRUE(train.able(OOMIYA, YOKOHAMA));
+	initAshiato();
 	EXPECT_FALSE(train.able(YOKOHAMA, OOSHIMA));
 }
 
@@ -76,8 +121,11 @@ TEST(TrainTest, Test2)
 	Train train;
 
 	EXPECT_TRUE(train.able(YOKOHAMA, TOKYO));
+	initAshiato();
 	EXPECT_TRUE(train.able(TOKYO, OOMIYA));
+	initAshiato();
 	EXPECT_TRUE(train.able(TOKYO, YOKOHAMA));
+	initAshiato();
 	EXPECT_TRUE(train.able(OOMIYA, TOKYO));
 }
 
@@ -85,16 +133,20 @@ TEST(TrainTest, Test3)
 {
 	Train train;
 	EXPECT_TRUE(train.able(OOMIYA, YOKOHAMA));
-
-//EXPECT_TRUE(train.checkTokyo(OOMIYA) && train.checkTokyo(YOKOHAMA));
 }
 
 TEST(TrainTest, TestManyStaion)
 {
 	Train train;
-	EXPECT_TRUE(train.able(KAWASAKI, AKIHABARA));
-	EXPECT_TRUE(train.able(AKABANE, KAWASAKI));
-	
+	for(int i=0; i<ENUM_STATION_SIZE; i++){
+		initAshiato();
+		if(i == OOSHIMA){
+			EXPECT_FALSE(train.able(AKABANE, i));
+		}
+		else if(i != KAWASAKI){
+			EXPECT_TRUE(train.able(KAWASAKI, i));
+		}
+	}
 }
 
 int main(int argc, char* argv[])
