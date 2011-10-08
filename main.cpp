b9847759fb302034b03f4712dd676b578724b54a
Teppei Hirose <teppeihi@gmail.com>
2011-10-08 10:58:15 +0900

    Add sub Function.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,11 @@ int add(int x, int y)
     return x + y;
 }
 
+int sub(int x, int y)
+{
+	return x-y;
+}
+
 TEST(AddTest, Test1)
 {
     EXPECT_EQ(2, add(1, 1));
@@ -13,6 +18,10 @@ TEST(AddTest, Test1)
 TEST(AddTest, Test2)
 {
     EXPECT_EQ(3, add(1, 2));
+}
+TEST(SubTest, TestSub)
+{
+		EXPECT_EQ(1, sub(2,1));
 }
 
 int main(int argc, char* argv[])
