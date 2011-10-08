acdf4a72f44d91a2cedc19deb21fdaa3efe7305a
oreshio <oreshio+git@gmail.com>
2011-10-08 12:52:08 +0900

    リファクタリング

--- main.cpp
+++ main.cpp
@@ -1,18 +1,72 @@
 #include <gtest/gtest.h>
+#include <list>
 
-int add(int x, int y)
+bool canGo(const std::string& source, const std::string& destination)
 {
-    return x + y;
+	if ("Ohmiya" == source && "Yokohama" == destination)
+	{
+		return true;
+	}
+
+	if ("Yokohama" == source)
+	{
+		if ("Tokyo" == destination || "Ohmiya" == destination)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	if ("Tokyo" == source)
+	{
+		if ("Yokohama" == destination || "Ohmiya" == destination)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	if ("Ohmiya" == source)
+	{
+		if ("Yokohama" == destination || "Tokyo" == destination)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	return false;
 }
 
-TEST(AddTest, Test1)
+bool canGoWithList(const std::list<std::string>& sourceToDestination)
 {
-    EXPECT_EQ(2, add(1, 1));
+
+
+	return false;
 }
 
-TEST(AddTest, Test2)
+TEST(canGoTest, Test1)
 {
-    EXPECT_EQ(3, add(1, 2));
+	EXPECT_TRUE(canGo("Ohmiya", "Yokohama"));
+	EXPECT_TRUE(canGo("Yokohama", "Tokyo"));
+	EXPECT_TRUE(canGo("Tokyo", "Yokohama"));
+	EXPECT_TRUE(canGo("Tokyo", "Ohmiya"));
+	EXPECT_TRUE(canGo("Ohmiya", "Tokyo"));
+
+	EXPECT_FALSE(canGo("Yokohama", "OhShima"));
+}
+
+TEST(canGoTest, Test2)
+{
+	std::list<std::string> testData1;
+	testData1.push_back("Yokohama");
+	EXPECT_FALSE(canGoWithList(testData1));
+	testData1.push_back("Tokyo");
+	EXPECT_TRUE(canGoWithList(testData1));
+	testData1.push_back("Ohmiya");
+	EXPECT_TRUE(canGoWithList(testData1));
+
+	EXPECT_TRUE(canGoWithList(testData1));
 }
 
 int main(int argc, char* argv[])
