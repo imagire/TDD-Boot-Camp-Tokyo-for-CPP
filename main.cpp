50fce4a8f2eeade0697950488c5733082c94a405
i-onishi <asdwerxcv@gmail.com>
2011-10-08 14:12:08 +0900

    prob3

--- main.cpp
+++ main.cpp
@@ -23,3 +23,61 @@ int main(int argc, char* argv[])
 
 	return ret;
 }
+
+enum TRAIN_STATION
+{
+	OMIYA = 0,
+	YOKOHAMA,
+	TOKYO,
+	OSHIMA
+};
+
+bool cango(TRAIN_STATION start, TRAIN_STATION end)
+{
+
+	if(start == OMIYA)
+	{
+		if((end == YOKOHAMA) || (end == TOKYO))
+		{
+			return true;
+		}
+	}
+
+	if(start == TOKYO)
+	{
+		if((end == OMIYA)||(end == YOKOHAMA))
+		{
+			return true;
+		}
+	}
+
+
+	if(start == YOKOHAMA)
+	{
+		if((end == OMIYA)||(end == TOKYO))
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+TEST(Train, Move)
+{
+	EXPECT_TRUE(cango(OMIYA,YOKOHAMA));
+	EXPECT_FALSE(cango(OSHIMA,YOKOHAMA));
+}
+
+TEST(Train, Problem2)
+{
+	EXPECT_TRUE(cango(YOKOHAMA,TOKYO));
+	EXPECT_TRUE(cango(TOKYO,YOKOHAMA));
+	EXPECT_TRUE(cango(TOKYO,OMIYA));
+	EXPECT_TRUE(cango(OMIYA,TOKYO));	
+}
+
+TEST(Train, Problem3)
+{
+	EXPECT_TRUE(cango(YOKOHAMA,OMIYA));
+}
